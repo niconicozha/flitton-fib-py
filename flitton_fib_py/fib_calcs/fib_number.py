6a230a9420d82eef17626510183955157be40046
李树钊 <niconicozha@gmail.com>
2024-11-09 18:46:35 +0800

    implementing fibonacci calculation

---
+++ flitton_fib_py/fib_calcs/fib_number.py
@@ -0,0 +1,11 @@
+def recurring_fibonacci_number(number: int) -> int | None:
+    if number < 0:
+        return None
+    elif number <= 1:
+        return number
+    else:
+        return recurring_fibonacci_number(
+            number - 1
+        ) + recurring_fibonacci_number(  # noqa: E501
+            number - 2
+        )
