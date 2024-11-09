ba5db85d943c943f67506794b919d84f56ab9c7f
李树钊 <niconicozha@gmail.com>
2024-11-09 18:40:38 +0800

    adding setup to module

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="flitton_fib_py",
+    version="0.0.1",
+    author="Niconicozha",
+    author_email="niconicozha@gmail.com",
+    description="Calculates a Fibonacci number",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/niconicozha/flitton-fib-py",
+    install_requires=[],
+    packages=find_packages(exclude=("tests",)),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3",
+    tests_require=["pytest"],
+)
